68b10ef0bf5b73e4d50c52311e065a13af83bac4
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-07-22 15:59:16 +0100

    feat: add support for outer heading

--- prepare_merge.lua
+++ prepare_merge.lua
@@ -11,7 +11,15 @@ function Pandoc(doc)
   -- Number of levels to add above source headings
   local n = 1
 
-  -- Create path headers, if the option is set
+  -- Create outer header, if the option is set, and update n
+  local outer_header = nil
+  local outer_heading = pandoc.utils.stringify(doc.meta.outer_heading or "null")
+  if outer_heading ~= "null" then
+    outer_header = pandoc.Header(2, outer_heading)
+    n = n + 1
+  end
+
+  -- Create path headers, if the option is set, and update n
   local path_headers = {}
   local root = pandoc.utils.stringify(doc.meta.root or "null")
   local path_as_headings_root = pandoc.utils.stringify(doc.meta.path_as_headings_root or "null")
@@ -19,20 +27,26 @@ function Pandoc(doc)
     local root = pandoc.path.join({ root, path_as_headings_root })
     local dirname = pandoc.path.directory(pandoc.path.make_relative(PANDOC_STATE.input_files[1], root))
     local parts = pandoc.path.split(dirname)
-    n = n + #parts
     for i, part in ipairs(parts) do
-      local path_header = pandoc.Header(i+1, dir_as_title(part))
+      local path_header = pandoc.Header(i+n, dir_as_title(part))
       path_headers[#path_headers + 1] = path_header
     end
+    n = n + #parts
   end
 
-  -- Increase all heading levels by 1
+  -- Increase all heading levels by n+1
   local doc = doc:walk {
     Header = function(h)
       h.level = h.level + n + 1
       return h
     end
   }
+
+  -- Insert outer heading
+  if outer_header then
+    table.insert(doc.blocks, 1, pandoc.Para({}))
+    table.insert(doc.blocks, 1, outer_header)
+  end
 
   -- Insert the path as headings
   for i, path_header in ipairs(path_headers) do
