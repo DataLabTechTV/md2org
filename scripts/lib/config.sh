136df1d9412a98c46f34f6abcbd84953943021c4
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-07-31 14:20:50 +0100

    feat: add relative dir env vars from the base dir for data subdirs

--- scripts/lib/config.sh
+++ scripts/lib/config.sh
@@ -7,9 +7,12 @@ SCRIPTS_DIR="$BASE_DIR/scripts"
 LIB_DIR="$SCRIPTS_DIR/lib"
 FILTERS_DIR="$BASE_DIR/filters"
 
-DATA_DIR=$(readlink -f "$BASE_DIR/data")
+DATA_DIR="$(readlink -f "$BASE_DIR/data")"
 MD_DIR="$DATA_DIR/md"
 ORG_DIR="$DATA_DIR/org"
 ORG_REMAPPED_DIR="$DATA_DIR/org-remapped"
+REL_MD_DIR="$(realpath --relative-to="$BASE_DIR" "$MD_DIR")"
+REL_ORG_DIR="$(realpath --relative-to="$BASE_DIR" "$ORG_DIR")"
+REL_ORG_REMAPPED_DIR="$(realpath --relative-to="$BASE_DIR" "$ORG_REMAPPED_DIR")"
 
 META_PATH="$DATA_DIR/meta.duckdb"
