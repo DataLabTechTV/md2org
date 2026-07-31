ffbedaec52e28a993a15d4c1c244eed7efe15fda
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-07-31 13:01:22 +0100

    feat: excalidraw markdown conversion now works for png images
    Still failing:
    
    - SVG due to missing with/height
    - WebP due to being unsupported

--- scripts/excalidraw-md-to-native.sh
+++ scripts/excalidraw-md-to-native.sh
@@ -13,6 +13,7 @@ usage() {
 
 SCRIPT_DIR="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
 
+. "$SCRIPT_DIR/lib/config.sh"
 . "$SCRIPT_DIR/lib/logging.sh"
 
 src=$1
@@ -20,43 +21,44 @@ dst=$2
 
 log DEBUG "$src"
 
-tmpfile="$(mktemp)"
-awk "BEGIN { json=0 } /^\`\`\`/ { json=!json; next } json" "$src" |
+tmpfile="$(mktemp "/tmp/md2org.excalidraw.XXXXXXXXXX")"
+log DEBUG "tmpfile: $tmpfile"
+awk 'BEGIN { json=0 } /^```/ { json=!json; next } json' "$src" |
     tr -d "\n" |
     go-lz-string decompress -m base64 \
     >"$tmpfile"
 
-embed_tmpfile="$(mktemp)"
-awk "
+embed_tmpfile="$(mktemp "/tmp/md2org-excalidraw-files.jsonl.XXXXXXXXXX")"
+log DEBUG "embed_tmpfile: $embed_tmpfile"
+awk '
     BEGIN { files=0 }
     /^## Embedded Files/ { files=1; next }
     /^%%/ { files=0 }
     files
-" "$src" |
+' "$src" |
     tr -s "\n" |
     sed -e "s/://" -e "s/[][]//g" |
     while IFS= read -r line; do
-        hash=$(echo $line | cut -d" " -f1)
-        filename=$(echo $line | cut -d" " -f 2-)
-        path=$(find data/md/ -path "*$filename*")
+        hash=$(echo "$line" | cut -d" " -f1)
+        filename=$(echo "$line" | cut -d" " -f 2-)
+        path=$(find "$MD_DIR" -path "*$filename*")
         mime=$(file --brief --mime-type "$path")
         base64=$(printf "data:%s;base64,%s\n" \
             "$mime" \
             "$(base64 -w0 "$path")")
-        printf "{\"key\":\"%s\",\"value\":{\"mimeType\":\"%s\",\"id\":\"%s\",\"dataURL\":\"%s\"}}\n" \
-            "$hash" \
+        printf "{\"mimeType\":\"%s\",\"id\":\"%s\",\"dataURL\":\"%s\"}\n" \
             "$mime" \
             "$hash" \
             "$base64"
     done >"$embed_tmpfile"
 
-# # FIXME this doesnt work yet
-# jq -s --slurpfile entries <(jq -s "." "$embed_tmpfile") '
-#     .files = (
-#         .files + (
-#             $entries[0] | map({(.key): .value}) | add
-#         )
-#     )
-# ' "$tmpfile"
+jq --slurpfile entries <(jq -s '.' "$embed_tmpfile") '
+    .files = (
+        .files + (
+            $entries[0] | map({(.id): .}) | add
+        )
+    )
+' "$tmpfile" >"$dst"
 
-mv "$tmpfile" "$dst"
+rm -fv "$tmpfile"
+rm -fv "$embed_tmpfile"
