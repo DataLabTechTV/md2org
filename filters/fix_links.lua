7e3bd41cdf3f780ff77aebbc6dbfcd2006a8c0fe
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2026-08-01 17:51:51 +0100

    feat: normalize internal links
    Note that file links are still not normalized.

--- filters/fix_links.lua
+++ filters/fix_links.lua
@@ -1,3 +1,32 @@
+local accents = {
+  ["á"]="a", ["à"]="a", ["â"]="a", ["ä"]="a", ["ã"]="a", ["å"]="a",
+  ["Á"]="A", ["À"]="A", ["Â"]="A", ["Ä"]="A", ["Ã"]="A", ["Å"]="A",
+
+  ["é"]="e", ["è"]="e", ["ê"]="e", ["ë"]="e",
+  ["É"]="E", ["È"]="E", ["Ê"]="E", ["Ë"]="E",
+
+  ["í"]="i", ["ì"]="i", ["î"]="i", ["ï"]="i",
+  ["Í"]="I", ["Ì"]="I", ["Î"]="I", ["Ï"]="I",
+
+  ["ó"]="o", ["ò"]="o", ["ô"]="o", ["ö"]="o", ["õ"]="o",
+  ["Ó"]="O", ["Ò"]="O", ["Ô"]="O", ["Ö"]="O", ["Õ"]="O",
+
+  ["ú"]="u", ["ù"]="u", ["û"]="u", ["ü"]="u",
+  ["Ú"]="U", ["Ù"]="U", ["Û"]="U", ["Ü"]="U",
+
+  ["ç"]="c", ["Ç"]="C",
+  ["ñ"]="n", ["Ñ"]="N",
+}
+
+local function strip_accents(s)
+  return s:gsub(
+    "[%z\1-\127\194-\244][\128-\191]*",
+    function(c)
+      return accents[c] or c
+    end
+  )
+end
+
 local function urldecode(s)
   return s:gsub(
     "%%(%x%x)",
@@ -7,8 +36,18 @@ local function urldecode(s)
   )
 end
 
--- TODO either convert to kebab case here, or adapt using the paths from meta.duckdb
-local function kebab_case(s)
+local function to_normalized_org_path(path)
+  path = urldecode(path)
+  path = strip_accents(path)
+  path = path
+    :lower()
+    :gsub("([^%.])%.([^%.])", "%1-%2")
+    :gsub("(.*)%-(.*)$", "%1.org")
+    :gsub("[ %-%–—]+", "-")
+    :gsub("[',]", "")
+    :gsub("&", "and")
+
+  return path
 end
 
 function Link(link)
@@ -23,21 +62,20 @@ function Link(link)
     return link
   end
 
-  local input = PANDOC_STATE.input_files[1]:gsub("^data/md/", "")
+  -- print("CONTENT: " .. pandoc.utils.stringify(link.content))
+  -- print("TARGET: " .. link.target)
+  -- -- TODO fix file/image links
+  -- print("UPDATED: " .. link.target)
 
-  print("INPUT: " .. input)
-  print("TARGET: " .. link.target)
-  print("CONTENT: " .. pandoc.utils.stringify(link.content))
-
+  -- Fix internal links with a fragment
   local file, fragment = link.target:match("^(.-)#(.+)$")
   if file and fragment then
-    print("FILE + FRAGMENT: " .. file .. " :: " .. pandoc.utils.stringify(fragment))
-    link.target = urldecode(file) .. "::#" .. fragment
-    print("UPDATED: " .. link.target)
+    link.target = "file:" .. to_normalized_org_path(file) .. "::*" .. fragment
+    return link
   end
 
-  -- local path = urldecode(link.target)
-  -- print("PATH: " .. path)
+  -- Fix remaining internal links
+  link.target = to_normalized_org_path(link.target)
 
   return link
 end
